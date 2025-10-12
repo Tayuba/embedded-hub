143359a8fe0f3282ebcfe321846ff5bc32585845
Tahiru Ayuba <Ayuba.Tahiru@comlet.de>
2025-10-12 02:12:02 +0200

    Clean repository and remove old project files

--- tutorials/c_fundamentals/include/big_o.h
+++
@@ -1,6 +0,0 @@
-#ifndef BIG_O_H
-#define BIG_O_H
-
-void big_o_test(void);
-
-#endif
--- tutorials/c_fundamentals/include/bitwise_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef BITWISE_UTILS_H
-#define BITWISE_UTILS_H
-
-void demonstrate_bitwise_operations(void);
-
-#endif
